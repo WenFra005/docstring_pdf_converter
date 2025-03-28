f3f8cd0b73c6b6ccdca90bc519dda3adade70d81
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-28 17:27:41 -0300

    FEAT: Atualiza a geração de capa para incluir instituição, cidade e ano

--- docstring_pdf_converter/generate_cover.py
+++ docstring_pdf_converter/generate_cover.py
@@ -1,30 +1,40 @@
-import os
-
 from fpdf import FPDF
 
-def generate_cover (title: str, subtitle: str, author: str, output_file: str):
+def generate_cover(title: str, subtitle: str, institution: str, city: str, year: str, output_file: str):
     pdf = FPDF()
-    pdf.set_auto_page_break(auto=True, margin=20)
+    pdf.set_auto_page_break(auto=False)
     pdf.add_page()
 
     pdf.set_left_margin(30)
     pdf.set_top_margin(30)
     pdf.set_right_margin(20)
 
-    pdf.set_font("Times", "B", 16)
-    pdf.cell(0, 40, title, ln=True, align="C")
+    pdf.set_font("Times", "B", 12)
+    pdf.cell(0, 10, f"{institution.upper() if institution else 'AUTOR INDEPENDENTE'}", ln=True, align="C")
 
-    pdf.set_font("Times", "I", 14)
-    pdf.cell(0, 20, subtitle, ln=True, align="C")
+    # Pular 8 linhas
+    for _ in range(8):
+        pdf.ln(10)
 
+    pdf.cell(0, 10, f"{title.upper()}", ln=True, align="C")
     pdf.set_font("Times", "", 12)
-    pdf.ln(50)
-    pdf.cell(0, 10, f"Author: {author}", ln=True, align="C")
+    pdf.cell(0, 10, f"{subtitle}", ln=True, align="C")
+
+    # Calcular a posição para a cidade e o ano
+    pdf.set_font("Times", "B", 12)
+    page_height = pdf.h - pdf.b_margin
+    pdf.set_y(page_height - 20)
+    pdf.set_font("Times", "", 12)
+    pdf.cell(0, 10, f"{city.upper()}", ln=True, align="C")
+    pdf.cell(0, 10, f"{year}", ln=True, align="C")
 
     pdf.output(output_file)
 
-    generate_cover(
-        "Documentação do projeto",
-        "Conversão de docstring para PDF",
-        "Wendell Francisco",
-        "capa.pdf")
+generate_cover(
+    "Documentação do projeto",
+    "Conversão de docstring para PDF",
+    "",
+    "São Paulo",
+    "2025",
+    "capa.pdf"
+)
