e806191988de3f1db4dab20eb4ffba89dbcedbf2
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-04-02 17:12:13 -0300

    FEAT: Adiciona arquivo de exemplo com função e classe demonstrativas

---
+++ docstring_pdf_converter/exemplo.py
@@ -0,0 +1,37 @@
+# arquivo_exemplo.py
+
+def funcao_exemplo(param1, param2):
+    """
+    Esta é uma função de exemplo.
+
+    Args:
+        param1 (int): O primeiro parâmetro.
+        param2 (str): O segundo parâmetro.
+
+    Returns:
+        bool: O resultado da operação.
+    """
+    return True
+
+class ClasseExemplo:
+    """
+    Esta é uma classe de exemplo.
+    """
+
+    def __init__(self, atributo1):
+        """
+        Inicializa a ClasseExemplo.
+
+        Args:
+            atributo1 (str): O primeiro atributo.
+        """
+        self.atributo1 = atributo1
+
+    def metodo_exemplo(self):
+        """
+        Este é um método de exemplo.
+
+        Returns:
+            str: Uma mensagem de exemplo.
+        """
+        return "Este é um método de exemplo."
