ae2de17368febac97d18c81063273c81736ba28a
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-04-05 17:45:49 -0300

    FEAT: Adiciona metadados ao setup.py e cria configuração de estilo de código

--- setup.py
+++ setup.py
@@ -14,4 +14,24 @@ setup(
             "docstring-pdf-converter = docstring_pdf_converter.main.py",
         ]
     },
+    description="Uma ferramenta para converter docstrings de módulos Python em PDFs formatados.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/WenFra005/docstring_pdf_converter",
+    author="Wendell Francisco",
+    author_email="wendellfrancisco2005@hotmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={
+        "": ["*.txt", "*.py"],
+        "docstring_pdf_converter": ["data/*.txt"],
+    },
+    tests_require=["pytest"]
 )
