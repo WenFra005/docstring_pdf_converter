3a6e5e723d89712e4e79c067547093a8b0428228
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-04-13 15:23:50 -0300

    FEAT: Atualiza a versão do pacote para 1.0.0 no setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="docstring_pdf_converter",
-    version="0.2.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=["fpdf"],
     entry_points={
